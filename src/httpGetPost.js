2072ef963946b12106b1be2c901a47d5075d14ff
sink <1468709106@qq.com>
2020-08-29 22:46:28 +0800

    http

---
+++ src/httpGetPost.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+
+let method = {
+    // 获取get数据
+    GET: function getFn(req, res) {
+        const requestData = url.parse(req.url, true);
+        let urlPath = requestData.path;
+        let urlData = requestData.query;
+
+        let log = "==>urlPath:" + urlPath +"==>>urlData:"+ JSON.stringify(urlData);
+        console.log(log);
+        
+        res.write(log);
+        res.end();
+    },
+    // 获取post数据
+    POST: function postFn(req, res) {
+        let dataStr = '';
+        req.on('data', (chunk) => {
+            dataStr += chunk;
+        })
+        req.on('end', () => {
+            let parseData = querystring.parse(dataStr);
+            console.log("parseData:", parseData);
+            res.write(new Buffer(dataStr, "utf8"));
+            res.end();
+        })
+    }
+};
+
+http.createServer((request, respond) => {
+    let reqMethod = request.method;
+    method[reqMethod](request, respond);
+}).listen(3000, function () {
+    console.log('监听端口:%s', 3000);
+});
