7fe2ad157ef27337a7c989f339de1bedbf35d4af
sink <1468709106@qq.com>
2020-09-07 20:35:33 +0800

    flatarray

--- src/flatArray.js
+++ src/flatArray.js
@@ -1,6 +1,9 @@
-// 数组扁平化
+// 数组扁平化(变成一维数组)
 
-// 变成一维数组 方法一：递归， [].concat(...arr)
+/**
+ * 方法一：递归，核心 [].concat(...arr)
+ * @param {*} arr 
+ */
 function flatArray1(arr = []) {
     let result = [];
 
@@ -15,30 +18,46 @@ function flatArray1(arr = []) {
     return result;
 }
 
-let arr = [1, 2, 3, [1, 2, [5], [1, 2, 3]]];
-console.log(flatArray1(arr));
-
-// 方法二：利用toString() 扁平化数组
+/**
+ * 方法二：利用toString()
+ * @param {*} arr 
+ */
 function flatArray2(arr) {
     // 先把多维数组先转换为字符串，再用逗号分隔符将字符串对象分割成字符串数组
     return arr.toString().split(',').map(item => parseFloat(item));
 }
-console.log(flatArray2(arr));
 
-// 方法三：利用JSON.stringify() 扁平化数组
+/**
+ * 方法三：利用JSON.stringify()
+ * @param {*} arr 
+ */
 function flatArray3(arr) {
     // 先序列化，替换掉数组的中括号，逗号分隔转成数组
     return JSON.stringify(arr).replace(/(\[|\])/g, '').split(',').map(item=>parseFloat(item));
 }
-console.log(flatArray3(arr));
 
-// 方法四：用reduce实现，[].concat(...arr)
+/**
+ * 方法四：用reduce实现，核心[].concat(...arr)
+ * @param {*} arr 
+ */
 function flatArray4(arr) {
     return arr.reduce((pre, cur) => {
         return pre.concat(isArray(cur) ? flatArray4(cur) : cur);
     }, []);
 }
-console.log(flatArray4(arr));
+
+/**
+ * 测试
+ */
+function test() {
+    let arr = [1, 2, 3, 4, 5, 7.5, [8, 4, [5], [1, 2, 3]]];
+    console.log(flatArray1(arr));
+    console.log(flatArray2(arr));
+    console.log(flatArray3(arr));
+    console.log(flatArray4(arr));
+}
+
+test();
 
 /**=========================================================== */
 /**
