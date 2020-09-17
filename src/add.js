eb44933dd461554c06b1fda4abfabe8a0e6a4c3b
lsx <1468709106@qq.com>
2020-09-17 22:40:57 +0800

    add

---
+++ src/add.js
@@ -0,0 +1,28 @@
+/**
+ * 柯里化 实现一个add 函数
+ * @param {*} fn 
+ * @param  {...any} args 
+ */
+const curry = (fn, ...args) => {
+    console.log(args.length, fn.length)
+    // 函数的参数个数可以直接通过函数数的.length属性来访问
+    return args.length >= fn.length
+        // 传入的参数大于等于原始函数fn的参数个数，则直接执行该函数
+        ? fn(...args)
+        /**
+         * 传入的参数小于原始函数fn的参数个数时
+         * 则继续对当前函数进行柯里化，返回一个接受所有参数（当前参数和剩余参数） 的函数
+         */
+        : (..._args) => curry(fn, ...args, ..._args);
+}
+
+function add1(x, y, z) {
+    return x + y + z;
+}
+
+const add = curry(add1);
+
+console.log(add(1, 2, 3)); // 6
+console.log(add(1)(2)(3)); // 6
+console.log(add(1, 2)(3)); // 6
+console.log(add(1)(2, 3)); // 6
