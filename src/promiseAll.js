ace2c7c46e0d8e3894ce3b4bc5f4efeeb6514880
lsx <1468709106@qq.com>
2020-11-01 20:12:16 +0800

    all

---
+++ src/promiseAll.js
@@ -0,0 +1,35 @@
+/**
+ * promise的all实现
+ * 
+ * 仅考虑 promises 传入的是数组的情况时
+ * 
+ * @param {*} promise 
+ */
+Promise.all = function (promise) {
+    // 将iterator转换为数组
+    let promises = Array.from(promise);
+
+    return new Promise((resolve, reject) => {
+        // 如果数组长度为0则返回空数组
+        if (promises.length === 0) {
+            resolve([]);
+        } else {
+            let result = []; // 存放已成功的异步操作
+            let index = 0; // 记录已成功的操作数
+            for (let i = 0; i < promises.length; i++) {
+                // 执行每一个promise
+                Promise.resolve(promises[i]).then(data => {
+                    result[i] = data;
+                    // 所有promises状态都是fulfilled才返回
+                    if (++index === promises.length) {
+                        resolve(result);
+                    }
+                }, err => {
+                    reject(err);
+                    return;
+                });
+            }
+        }
+
+    });
+}
