8cbcfe462e6707556199b65fc70fe46e673057e0
lsx <1468709106@qq.com>
2020-12-12 21:57:29 +0800

    blue

--- src/blueRedLight.js
+++ src/blueRedLight.js
@@ -2,13 +2,13 @@
 /**
  * setTimeout实现休眠或等待功能的函数
  * @param {*} delayTime 延迟的时间
- * @param {*} result 一些参数
+ * @param {*} color 一些参数
  */
-function sleep(delayTime, result) {
+function sleep(delayTime, color) {
     return new Promise(function(resolve, reject) {
         setTimeout(() => { 
-            console.log(`当前是  ${result}  灯`);
-            resolve();
+            // console.log(`当前是  ${result}  灯`);
+            resolve(color);
          }, delayTime);
     });
 }
@@ -23,9 +23,14 @@ function sleep(delayTime, result) {
  */
 async function colorTest() {
     while (1) {
-        await sleep(1000, '红');
-        await sleep(2000, '绿');
-        await sleep(3000, '黄');
+        const red = await sleep(1000, '红');
+        console.log(`当前是 ${red} 灯`)
+
+        const green = await sleep(2000, '绿');
+        console.log(`当前是 ${green} 灯`)
+
+        const yellow = await sleep(3000, '黄');
+        console.log(`当前是 ${yellow} 灯`)
     }
 }
 
