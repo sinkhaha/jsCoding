d0fbfa2a790fc9350c1a0d57095909f11c031435
lsx <1468709106@qq.com>
2020-12-18 21:00:10 +0800

    clone

---
+++ src/clone.js
@@ -0,0 +1,11 @@
+/**
+ * 浅拷贝
+ * @param {*} obj 
+ */
+function copy(obj) {
+    let result = Array.isArray(obj) ? [] : {};
+
+    Object.keys(obj).forEach(key => result[key] = obj[key]);
+    
+    return result;
+}
