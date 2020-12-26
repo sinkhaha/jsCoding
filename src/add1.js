1e9de40c735642d293c244c45859eb30ee2b0b95
lsx <1468709106@qq.com>
2020-12-26 15:57:01 +0800

    curry

--- src/add1.js
+++ src/add1.js
@@ -6,18 +6,24 @@
  * @param {*} fn 
  * @param  {...any} args 
  */
-const curry = (fn, ...args) => {
-    console.log(`length=${args.length},args=${args}`, fn.length);
-    
-    // 函数的参数个数可以直接通过"函数的.length"属性来访问
-    return args.length >= fn.length
-        // 传入的参数大于等于原始函数fn的参数个数，则直接执行该函数
-        ? fn(...args)
-        /**
-         * 传入的参数小于原始函数fn的参数个数时
-         * 则继续对当前函数进行柯里化，返回一个接受所有参数（当前参数和剩余参数） 的函数
-         */
-        : (..._args) => curry(fn, ...args, ..._args);
+function curry(fn, currArgs) {
+    return function () {
+        // 数组化
+        let args = [].slice.call(arguments);
+
+        // 首次调用时，若未提供最后一个参数currArgs，则不用进行args的拼接
+        if (currArgs !== undefined) {
+            args = args.concat(currArgs);
+        }
+
+        // 递归调用
+        if (args.length < fn.length) {
+            return curry(fn, args);
+        }
+
+        // 递归出口
+        return fn.apply(null, args);
+    }
 }
 
 function add1(x, y, z) {
@@ -26,7 +32,7 @@ function add1(x, y, z) {
 
 const add = curry(add1);
 
-// console.log(add(1, 2, 3)); // 6
-// console.log(add(1)(2)(3)); // 6
+console.log(add(1, 2, 3)); // 6
+console.log(add(1)(2)(3)); // 6
 console.log(add(1, 2)(3)); // 6
-// console.log(add(1)(2, 3)); // 6
+console.log(add(1)(2, 3)); // 6
