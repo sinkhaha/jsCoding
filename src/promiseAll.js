3f2ddae574703f0d68aacd981c260916d11bc81e
lsx <1468709106@qq.com>
2021-01-18 21:32:01 +0800

    fix

--- src/promiseAll.js
+++ src/promiseAll.js
@@ -6,7 +6,7 @@
  * @param {*} promise 
  */
 Promise.all = function (promise) {
-    // 将iterator转换为数组
+    // 将iterator转换为数组，如果不是一个可迭代对象，不会报错，会返回空数组
     let promises = Array.from(promise);
 
     return new Promise((resolve, reject) => {
