804d01c5b08250947029bb346b70107b8282e804
lsx <1468709106@qq.com>
2021-01-18 22:02:01 +0800

    fix

---
+++ src/带权随机算法.js
@@ -0,0 +1,36 @@
+// const list =
+//     [
+//         {
+//             weigth: 10,
+//             val: 1
+//         },
+//         {
+//             weight: 55.5, // 为浮点数
+//             val: 2
+//         },
+//         {
+//             weight: 20,
+//             val: 3
+//         }
+//     ]
+
+/**
+ * 带权随机算法？
+ * 
+ * 参数是一个权重的随机数，返回一个随机数
+ * @param {*} list 
+ */
+function weightedRandom(list) {
+    // let randomList = [];
+
+    // for (var item of list) {
+    //     let weight = item.weight;
+    //     for (var j = 0; j < weight; j++) {
+    //         randomList.push(item.val);
+    //     }
+    // }
+
+    // let randomValue = randomList[Math.floor(Math.random() * randomList.length)];
+
+    // return randomValue;
+}
