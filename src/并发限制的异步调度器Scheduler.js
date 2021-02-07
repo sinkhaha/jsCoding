241128ecf5e9e0e970d9e3797e1a63f9d058e2c5
lsx <1468709106@qq.com>
2021-02-07 20:30:52 +0800

    scheduler

---
+++ src/并发限制的异步调度器Scheduler.js
@@ -0,0 +1,90 @@
+//JS实现一个带并发限制的异步调度器Scheduler,保证同时运行的任务最多有两个。
+//完善代码中Scheduler类,使得以下程序能正确输出：
+
+// class Scheduler {
+//     add(promiseCreator) { ... }
+
+//     // ...
+// }
+
+// const timeout = (time) => new Promise(resolve => {
+//     setTimeout(resolve, time)
+// })
+
+// const scheduler = new Scheduler()
+// const addTask = (time, order) => {
+//     scheduler.add(() => timeout(time))
+//         .then(() => console.log(order))
+// }
+
+// addTask(1000, '1')
+// addTask(500, '2')
+// addTask(300, '3')
+// addTask(400, '4')
+// output: 2 3 1 4
+
+// 一开始,1、2两个任务进入队列
+// 500ms时,2完成,输出2,任务3进队
+// 800ms时,3完成,输出3,任务4进队
+// 1000ms时,1完成,输出1
+// 1200ms时,4完成,输出4
+
+// 需要我们控制同时执行的promise个数，比如控制为2个，后面的所有promise都排队等待前面的执行完成
+class Scheduler {
+	constructor() {
+		this.tasks = [], // 待运行的任务
+        this.usingTask = [], // 正在运行的任务
+        this.maxNum = 2;
+    }
+    
+	// promiseCreator 是一个异步函数，return Promise
+	add(promiseCreator) {
+		return new Promise((resolve, reject) => {
+            promiseCreator.resolve = resolve
+            
+			if (this.usingTask.length < this.maxNum) {
+				this.usingRun(promiseCreator)
+			} else {
+				this.tasks.push(promiseCreator)
+			}
+		})
+	}
+
+	usingRun(promiseCreator) {
+        this.usingTask.push(promiseCreator)
+        
+		promiseCreator().then(() => {
+            promiseCreator.resolve()
+            this.usingMove(promiseCreator)
+			if (this.tasks.length > 0) {
+                // 递归
+				this.usingRun(this.tasks.shift())
+			}
+		})
+	}
+
+    /**
+     * 从队列中移除已经执行的函数
+     * @param {*} promiseCreator 
+     */
+	usingMove(promiseCreator) {
+		let index = this.usingTask.findIndex(promiseCreator)
+		this.usingTask.splice(index, 1)
+	}
+}
+
+const timeout = (time) => new Promise(resolve => {
+	setTimeout(resolve, time)
+})
+
+const scheduler = new Scheduler()
+
+const addTask = (time, order) => {
+	scheduler.add(() => timeout(time)).then(() => console.log(order))
+}
+
+addTask(1000, '1') 
+addTask(500, '2') 
+addTask(300, '3') 
+addTask(400, '4')
+// 2 3 1 4
