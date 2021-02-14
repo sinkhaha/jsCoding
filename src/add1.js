b4210d32db8745bd2c20124433d9598a565c240b
lsx <1468709106@qq.com>
2021-02-14 14:50:30 +0800

    fix

--- src/add1.js
+++ src/add1.js
@@ -3,6 +3,11 @@
  * 
  * 实现一个add 函数
  * 
+ * 需求：实现一个add方法，使计算结果能够满足如下预期：
+ * add(1)(2)(3) = 6;
+ * add(1, 2, 3)(4) = 10;
+ * add(1)(2)(3)(4)(5) = 15;
+ *
  * @param {*} fn 
  * @param  {...any} args 
  */
@@ -11,9 +16,11 @@ function curry(fn, currArgs) {
         // 数组化
         let args = [].slice.call(arguments);
 
+        console.log('args1', args);
         // 首次调用时，若未提供最后一个参数currArgs，则不用进行args的拼接
         if (currArgs !== undefined) {
             args = args.concat(currArgs);
+            console.log('args2', args);
         }
 
         // 递归调用
