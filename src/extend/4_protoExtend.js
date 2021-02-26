14e94a9aa63a28326c61a4d310e857cf1326eb9a
sinkhaha <1468709106@qq.com>
2021-02-26 16:17:21 +0800

    Update 4_protoExtend.js

--- src/extend/4_protoExtend.js
+++ src/extend/4_protoExtend.js
@@ -9,6 +9,7 @@
  * 缺点：
  * 1. 所有实例都会继承原型上的属性
  * 2. 无法实现复用（新实例属性都是后面添加的）
+ * 3. 所有新实例都会共享父类实例的属性(某个实例修改父类引用类型的属性时，其他实例该属性也会改变)(和原型链继承一样)
  *
  * @param {*} obj 
  */
@@ -22,9 +23,15 @@ var person = {
     name: 'lisi',
     friends: ['zhangsan', 'wangwu']
 };
+
 var anotherPerson = create(person);
+var anotherPerson2 = create(person);
 
 console.log(anotherPerson.name); // lisi 继承了父类函数的属性
+console.log(anotherPerson2.name); // lisi 继承了父类函数的属性，不是引用类型，不会改变
 
 anotherPerson.friends.push('haha');
-console.log(person.friends); // [ 'zhangsan', 'wangwu', 'haha' ]
+// 引用类型，因为是共享，所以值改变了
+console.log(person.friends);  //  ["zhangsan", "wangwu", "haha"]
+console.log(anotherPerson.friends);  //  ["zhangsan", "wangwu", "haha"]
+console.log(anotherPerson2.friends);  //  ["zhangsan", "wangwu", "haha"]
