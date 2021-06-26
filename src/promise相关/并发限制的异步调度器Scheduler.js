c697f627eb1f5b534381074e922fe33ad63cc9b3
lsx <1468709106@qq.com>
2021-06-26 17:33:19 +0800

    fix

--- src/promise相关/并发限制的异步调度器Scheduler.js
+++ src/promise相关/并发限制的异步调度器Scheduler.js
@@ -1,5 +1,5 @@
-//JS实现一个带并发限制的异步调度器Scheduler，保证同时运行的任务最多有两个
-//完善代码中Scheduler类,使得以下程序能正确输出：
+// JS实现一个带并发限制的异步调度器Scheduler，保证同时运行的任务最多有两个
+// 完善代码中Scheduler类,使得以下程序能正确输出：
 
 // class Scheduler {
 //     add(promiseCreator) { ... }
